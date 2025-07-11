80c2fa58f8755300c2a7aaba08c6b3bcafd9ef57
joaopfduarte <joaopedrofduarte@outlook.com>
2025-07-10 23:39:58 -0300

    feat: first implementation version

--- ex4i.m
+++ ex4i.m
@@ -1,7 +1,10 @@
 pkg load control
-num = [4]; den = [1 6 11 6];
 
+num = [4]; den = [1 6 11 6];
+T = 0.1;
 G = tf(num, den);
+
+Gz = c2d(G, T, 'zoh');
 
 rlocus(G);
 
@@ -11,14 +14,33 @@ step(sys_osc);
 
 Tcr = 2.5;
 
-Kp = 0.6 * Tcr;
+Kp = 0.6 * Kcr;
 Ti = 0.5 * Tcr;;
 Td = 0.125 * Tcr;
 
+%Analógico
 s = tf('s');
 Gc = Kp * (1 + (1/(Ti * s)) + (Td * s));
-
-figure;
 sys_cl = feedback(Gc * G, 1);
-step(sys_cl);
+
+
+Kd = Kp * Td / T;
+Ki = Kp * T / Ti;
+Kpi = Kp / (1 - T / (2 * Ti));
+
+
+%Digital
+z = tf('z', T);
+Gcz = Kpi + Ki * T * z / (z - 1) + Kd * (z - 1) / (T * z);
+sys_clz = feedback(Gcz * Gz, 1);
+
+
+t = 0:T:10;
+figure;
+step(sys_cl, sys_clz, t);
+legend("Contínuo", "Discreto");
 title("Resposta ao degrau");
+xlabel("Tempo (s)");
+ylabel("Saída");
+grid on;
+
